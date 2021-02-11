6eb7355bb3a9b36600d4a6746434fb755b30f022
Oleksandr2309 <62704353+Oleksandr2309@users.noreply.github.com>
2021-02-11 15:37:41 +0200

    Create MyReconfScriptTest.sh

---
+++ MyReconfScriptTest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#echo $Hello
+#echo $HelloValue
+
+T1="$HelloValue"
+T2="$HelloEnv"
+        if [ "$T1" != "$T2" ]; then
+dotnet /app/src/DotNetCoreHelloFromAppSettings/test1/DotNetCoreHelloFromAppSettings.dll
+fi 
+if [ "$T1" == "$T2" ]; then
+cd /app/src/DotNetCoreHelloFromAppSettings/default/ && dotnet DotNetCoreHelloFromAppSettings.dll
+fi
+
+echo $HelloEnv
